0be0c6fb9d3784fae4381895b00b2ae8ed818dd6
mia1sabel <miacarrascox12@gmail.com>
2026-07-31 00:24:07 -0400

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://miaisabel.github.io',
-  base: '/45-minute-media',
+  site: 'https://45minutemedia.com',
+  base: '/',
 });
